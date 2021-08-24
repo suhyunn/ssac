cba7f25f29099ea0d176dfb8b19a0faa108b52ce
so__ohyun <39949713+suhyunn@users.noreply.github.com>
2021-08-24 18:04:43 +0900

    0824

---
+++ js/function.js
@@ -0,0 +1,56 @@
+let a = 2;
+const b = 5;
+
+a = 3;
+// b = 3;
+
+console.log(a);
+console.log(b);
+
+if (a >= 3) {
+  console.log(">=true");
+} else {
+  console.log("<false");
+}
+
+if (a < 3) {
+  console.log(">=true");
+} else {
+  console.log("<false");
+}
+
+if (a - 4) {
+  console.log("-ture");
+} else {
+  console.log("-false");
+}
+
+if (b >= 5 && b <= 10) {
+  console.log("&&true");
+} else {
+  console.log("&&false");
+}
+if (!(b > 3)) {
+  console.log("!ture");
+} else {
+  console.log("!false");
+}
+if (1 == "1") {
+  console.log("==true");
+} else {
+  console.log("==false");
+}
+if (1 === "1") {
+  console.log("==true");
+} else {
+  console.log("==false");
+}
+let c = 3;
+c = c + 1;
+console.log(c);
+let d = 3;
+d += 1;
+console.log(d);
+let e = 3;
+e++;
+console.log(e);
